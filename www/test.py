fb4ae3e49085f594e362901a80021dd6a11eefc1
kangleran <rankangle123@163.com>
2017-07-07 16:14:43 +0800

    day04

---
+++ www/test.py
@@ -0,0 +1,14 @@
+import orm,asyncio
+from models import User, Blog, Comment
+
+@asyncio.coroutine
+def test(loop):
+    yield from orm.create_pool(loop=loop,user='root', password='a871544728', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    yield from u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
