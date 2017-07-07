f7e903e0e882a4424f7373400c3eb3cda98858cd
kangleran <rankangle123@163.com>
2017-07-07 15:35:20 +0800

    modify app.py

--- www/app.py
+++ www/app.py
@@ -3,14 +3,14 @@ import asyncio,os,json,time
 from datetime import datetime
 from aiohttp import web
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+    return web.Response(body=b'<h1>hello,hrf</h1>',content_type='text/html')
 
 @asyncio.coroutine
 def init(loop):
     app=web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    srv=yield from loop.create_server(app.make_handler(),'10.154.74.8',80)
+    logging.info('server started at http://10.154.74.8:80...')
     return srv
 
 loop=asyncio.get_event_loop()
